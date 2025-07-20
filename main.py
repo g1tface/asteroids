fb33cea354387ba2b1555c14335d51d39783a83a
g1tface <jhmei0895@gmail.com>
2025-07-20 14:13:25 -0500

    more progress

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
     
 
